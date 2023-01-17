edf38ffe7b24585d3091be715a3be16b44463ad0
polux0 <afidelo@protonmail.com>
2023-01-18 00:19:31 +0100

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css"
 import type { AppProps } from 'next/app'
 
+// chains that are going to be supported
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
