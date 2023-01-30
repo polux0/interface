28b64e89946c377e1109518f93bc3212e75814cb
polux0 <afidelo@protonmail.com>
2023-01-31 00:26:20 +0100

    fix: resolved bug in comparison of `amountIn` and `currentAllowance` & code clean-up

--- components/exchange/exchange.tsx
+++ components/exchange/exchange.tsx
@@ -2,11 +2,12 @@
 import exchangeFrom from '@/styles/exchange/ExchangeFrom.module.css'
 import exchangeStyles from "@/styles/exchange/Exchange.module.css"
 import exchangeTo from '@/styles/exchange/ExchangeTo.module.css'
+import exchangeButton from "@/styles/exchange/ExchangeButton.module.css"
 import ExchangeButton from './exchangebutton'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useEffect, useState } from "react";
 import { ethers } from 'ethers';
-import { useAccount, useContractRead } from 'wagmi'
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite, useProvider } from 'wagmi'
 import { agencyStableAbi, agencyUsdcAmmRouterAbi } from '../../contracts/abis'
 import useDebounce from '../../hooks/Debounce';
 import { BigNumber } from 'ethers';
@@ -16,35 +17,95 @@ export default function Exchange({...props}){
     // technical debt
     // 2. create config for contracts ( hooks )
     const account = useAccount();
+    const provider = useProvider();
+
     
     const [ amount, setAmount ] = useState("0");
     const [ amountWei, setAmountWei ] = useState("0");
     const [ currentAllowance, setCurrentAllowance ] = useState("0");
     const [ expectedAmount, setExpectedAmount ] = useState("0");
+    const [ deadline, setDeadline ] = useState(0);
 
     const debouncedInputAmount = useDebounce(amount, 800);
+    const debouncedDeadline = useDebounce(deadline, 800);
 
+    // technical debt 
+    // user should set it's `deadline` as well as `slippage`
+    const defaultDeadline = 7200000;
+
+    const currentAllowanceNormalized = ethers.utils.parseUnits(currentAllowance.toString(), "ether").toString();
+    const amountWeiNormalized = ethers.utils.parseUnits(amountWei.toString(), "ether").toString();
+    const amountMinOutWeiValue = BigNumber.from(amountWei).sub(BigNumber.from(amountWei).div(10));
+
+    // technical debt - move all of those functions to separate module
+    // 🎥 agency stable deployed at 0x531734989A71f78054450BbfEB3C70BA3BffEf2c
+    // 🎥 agency usdc amm router deployed at 0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5
     // fetch allowance
     const { refetch: fetchAllowance } = useContractRead({
-        address: '0x97f35E291f65711CC8f48F9f92748FE17C5AF1Da',
+        address: '0x531734989A71f78054450BbfEB3C70BA3BffEf2c',
         abi: agencyStableAbi,
         functionName: 'allowance',
-        args:[account.address, "0x3a6315B9BFeCF72e39f8327DA6cCBf93473608B8"],
+        args:[account.address, "0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5"],
         enabled: false,
     })
     // fetch quote
     const { refetch: fetchQuote } = useContractRead({
-        address: '0x3a6315B9BFeCF72e39f8327DA6cCBf93473608B8',
+        address: '0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5',
         abi: agencyUsdcAmmRouterAbi,
         functionName: 'swapExactFraxForTempleQuote',
         args:[amountWei],
         enabled: false,
     })
+    // increase allowance
+    const { config: increaseAllowanceConfig } = usePrepareContractWrite({
+        address: '0x531734989A71f78054450BbfEB3C70BA3BffEf2c',
+        abi: agencyStableAbi,
+        functionName: 'increaseAllowance',
+        args: ["0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5", amountWeiNormalized],
+    })
+    // swapExactFraxForTemple
+    const { config: swapExactFraxForTempleConfig } = usePrepareContractWrite({
+        address: '0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5',
+        abi: agencyUsdcAmmRouterAbi,
+        functionName: 'swapExactFraxForTemple',
+        args: [amountWeiNormalized, amountMinOutWeiValue, account.address, debouncedDeadline],
+        // not sure if necessary
+        overrides: {gasLimit: BigNumber.from(600000)}
+    })
+    // config of contractWrite 
+    const { write: increaseAllowanceWrite, data: increaseAllowanceData, error: increaseAllowanceError, isLoading: increaseAllowanceIsLoading, isError: increaseAllowanceIsError, isSuccess: increaseAllowanceIsSuccess } = useContractWrite(increaseAllowanceConfig)
+    const { write: swapExactFraxForTempleWrite, data: swapExactFraxForTempleWriteData, error: swapExactFraxForTempleWriteError, isLoading: swapExactFraxForTempleWriteIsLoading, isError: swapExactFraxForTempleWriteIsError, isSuccess: swapExactFraxForTempleWriteIsSuccess } = useContractWrite(swapExactFraxForTempleConfig)
+
+    if(increaseAllowanceError){
+        console.log('Error with `usePrepareContractWriteIncreaseAllowance`: ', increaseAllowanceError)
+    }
+    if(swapExactFraxForTempleWriteError){
+        console.log('Error with `usePrepareContractWriteswapExactFraxForTempleWrite`: ', swapExactFraxForTempleWriteError)
+    }
+    const increaseAllowanceOrSwap = function(){
+        // technical debt
+        // question should we put `currentAllowanceNormalized` && `amountNormalized` as part of state
+        const currentAllowanceNormalized = BigNumber.from(currentAllowance).toString();
+        const amountNormalized = ethers.utils.parseEther(amount).toString()
+        const isCurrentAllowanceBiggerOrEqualToAmount = (BigNumber.from(currentAllowanceNormalized)).gte(BigNumber.from(amountNormalized));
+        return isCurrentAllowanceBiggerOrEqualToAmount ? "Swap" : "Increase allowance";
+    }
+    const increaseAllowance = function (){
+        increaseAllowanceWrite?.();
+        
+        // after we set allowance, increase current allowance to new value! and button will rerender itself
+    }
+    const increaseAllowanceOrSwapWrite = function(){
+
+        const result = increaseAllowanceOrSwap()
+        result === "Swap" ? swapExactFraxForTempleWrite?.() : increaseAllowance();
+    }
     useEffect(
         () => {
           if (debouncedInputAmount) {
             fetchAllowance?.().then(currentAllowancePromise =>{
                 const result = currentAllowancePromise?.data as string
+                console.log("amount: ", amountWei)
                 setCurrentAllowance(result);
             })
             fetchQuote?.().then(quote => {
@@ -62,13 +123,16 @@ export default function Exchange({...props}){
                 }
                 setExpectedAmount(amountOut.toString())
             });
+            const currentBlockNumber = provider.getBlockNumber().then(currentBlockNumber => { return currentBlockNumber });
+            provider.getBlock(currentBlockNumber).then(currentBlock => setDeadline(currentBlock.timestamp + defaultDeadline));
           } else {
             setCurrentAllowance("0");
             setExpectedAmount("0")
+            setDeadline(0);
           }
         },
-        [debouncedInputAmount, fetchAllowance, fetchQuote] // Only call effect if debounced search term changes
-      );
+        [debouncedInputAmount, fetchAllowance, fetchQuote, setDeadline, provider] // Only call effect if debounced search term changes
+    );
     async function amountHandler(event: any){
         const amount: any = event.target.value;
         let amountWei;
@@ -108,7 +172,9 @@ export default function Exchange({...props}){
             {/* </ExchangeTo> */}   
             <ConnectButton></ConnectButton>
             {/* here we pass wei value */}
-            <ExchangeButton amountWei = {amountWei} currentAllowance={currentAllowance}></ExchangeButton> 
+            {/* ExchangeButton */}
+            <button onClick={increaseAllowanceOrSwapWrite} className={exchangeButton.button} >{increaseAllowanceOrSwap()}</button>
+            {/* <ExchangeButton amountWei = {amountWei} currentAllowance={currentAllowance}></ExchangeButton>  */}
             {/* {isWalletConnected(walletConnected)} */}
         </div>
     )
--- components/exchange/exchangebutton.tsx
+++ components/exchange/exchangebutton.tsx
@@ -1,76 +1,9 @@
-import { agencyStableAbi, agencyUsdcAmmRouterAbi } from "@/contracts/abis";
-import exchangeButton from "@/styles/exchange/ExchangeButton.module.css"
-import { usePrepareContractWrite, useContractWrite, useWaitForTransaction, useAccount, useProvider} from "wagmi"
 import { useAddRecentTransaction } from '@rainbow-me/rainbowkit';
-import { useEffect, useState } from "react";
-import { BigNumber, ethers } from "ethers";
-import useDebounce from '../../hooks/Debounce';
-
-
+import {  useState } from "react";
 
 export default function ExchangeButton({...props}){
     const [deadline, setDeadline] = useState(0);
     const {amountWei, currentAllowance} = props;
-    console.log('amountWei:', amountWei)
-    // technical debt
-    const amountWeiNormalized = ethers.utils.parseUnits(amountWei.toString(), "ether").toString();
-    console.log('amountWeiNormalized:', amountWeiNormalized)
-
-    const debouncedDeadline = useDebounce(deadline, 800);
-
-    // technical debt 
-    // user should set it's deadline
-    const defaultDeadline = 7200000;
-    // default slippage
-    const amountMinOutWeiValue = BigNumber.from(amountWei).sub(BigNumber.from(amountWei).div(10));
-
-    const account = useAccount();
-    console.log('account: ', account)
-    const provider = useProvider();
-
-    const addRecentTransaction = useAddRecentTransaction();
-
-    // technical debt
-    // const prepareContractWrite = (functionName: string) => {
-    //     // getContractConfigBasedOnFunctionName()
-
-    // }
-    const { config: increaseAllowanceConfig } = usePrepareContractWrite({
-        address: '0x97f35E291f65711CC8f48F9f92748FE17C5AF1Da',
-        abi: agencyStableAbi,
-        functionName: 'increaseAllowance',
-        args: ["0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5", amountWeiNormalized],
-    })
-
-    const { config: swapExactFraxForTempleConfig } = usePrepareContractWrite({
-        address: '0x4d0E552aAc0370b68A23B5b00bd96f8e3FF556C5',
-        abi: agencyUsdcAmmRouterAbi,
-        functionName: 'swapExactFraxForTemple',
-        args: [amountWei, amountMinOutWeiValue, account.address, debouncedDeadline],
-        // not sure if necessary
-        overrides: {gasLimit: BigNumber.from(600000)}
-    })
-
-    useEffect(() => {
-        const currentBlockNumber = provider.getBlockNumber().then(currentBlockNumber => { return currentBlockNumber });
-        provider.getBlock(currentBlockNumber).then(currentBlock => setDeadline(currentBlock.timestamp + defaultDeadline));
-        return () => {
-            setDeadline(0);
-        };
-    }, [setDeadline, provider]);
-
-    // console.log('block.timestamp fetched: ', deadline)
-    // let swapTx = await contract.swapExactFraxForTemple(amountInWeiValue, amountMinOutWeiValue, account, deadline, {gasLimit: 600000});
-
-    const { write: increaseAllowanceWrite, data: increaseAllowanceData, error: increaseAllowanceError, isLoading: increaseAllowanceIsLoading, isError: increaseAllowanceIsError, isSuccess: increaseAllowanceIsSuccess } = useContractWrite(increaseAllowanceConfig)
-    const { write: swapExactFraxForTempleWrite, data: swapExactFraxForTempleWriteData, error: swapExactFraxForTempleWriteError, isLoading: swapExactFraxForTempleWriteIsLoading, isError: swapExactFraxForTempleWriteIsError, isSuccess: swapExactFraxForTempleWriteIsSuccess } = useContractWrite(swapExactFraxForTempleConfig)
-
-    if(increaseAllowanceError){
-        console.log('Error with `usePrepareContractWriteIncreaseAllowance`: ', increaseAllowanceError)
-    }
-    if(swapExactFraxForTempleWriteError){
-        console.log('Error with `usePrepareContractWriteswapExactFraxForTempleWrite`: ', swapExactFraxForTempleWriteError)
-    }
     // Technical debt
     // Think how this should be applied
     // useWaitForTransaction({
@@ -84,17 +17,9 @@ export default function ExchangeButton({...props}){
     // }
     // })
     // technical debt - once transaction is finished we should rerender the component
-    const increaseAllowanceOrSwap = function(){
-        return amountWei <= currentAllowance ? "Swap" : "Increase allowance";
-    }
-    const increaseAllowanceOrSwapWrite = function(){
-
-        const result = increaseAllowanceOrSwap()
-        result === "Swap" ? swapExactFraxForTempleWrite?.() : increaseAllowanceWrite?.();
-    }
     return(
         <div>
-            <button onClick={increaseAllowanceOrSwapWrite}className={exchangeButton.button}>{increaseAllowanceOrSwap()}</button>
+            {/* <button onClick={increaseAllowanceOrSwapWrite}className={exchangeButton.button}>{increaseAllowanceOrSwap()}</button> */}
         </div>
     )
 }
