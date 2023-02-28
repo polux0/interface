324a0bc5d4f3d00bf9779de28cc8a2c20c35cd08
polux0 <afidelo@protonmail.com>
2023-02-28 20:37:55 +0100

    fix: removed redundant code

--- components/exchange/exchange.tsx
+++ components/exchange/exchange.tsx
@@ -137,7 +137,7 @@ export default function Exchange({...props}){
     // user should set it's `deadline` as well as `slippage`
 
     let currentAllowanceNormalized;
-    let amountWeiNormalized;
+    let amountWeiNormalized: unknown;
     let amountMinOutWeiValue;
     try {
          currentAllowanceNormalized = ethers.utils.parseUnits(currentAllowance.toString(), "ether").toString();
@@ -379,7 +379,10 @@ export default function Exchange({...props}){
                   </div>
               </div>
               <div className="w-5/6 h-2/6 rounded-2xl border-4 border-black border-solid">
-                <button className="w-full text-black bg-white h-full rounded-2xl" onClick={isDisconnected? openConnectModal : enoughStables(data?.value.toString(), amountWeiNormalized) ? increaseAllowanceOrSwapWrite : test()}>{ isDisconnected? "Connect wallet" : enoughStables(data?.value.toString(), amountWeiNormalized) ? increaseAllowanceOrSwap(): "Insufficient USDC amount"}</button>
+                <button className="w-full text-black bg-white h-full rounded-2xl" 
+                        onClick={() => {isDisconnected? openConnectModal : enoughStables(data?.value.toString(), amountWeiNormalized) ? increaseAllowanceOrSwapWrite : test()}}>
+                        { isDisconnected? "Connect wallet" : enoughStables(data?.value.toString(), amountWeiNormalized) ? increaseAllowanceOrSwap(): "Insufficient USDC amount"}
+                </button>
               </div>
             </div>
           </div>
--- helpers/validation.ts
+++ helpers/validation.ts
@@ -1,8 +1,5 @@
 import { BigNumber } from "ethers";
 
 export default function enoughStables(balance: any, desiredAmount: any){
-    console.log("balance: ", balance)
-    console.log("desired amount: ", desiredAmount)
-    console.log("balance > desiredAmount: ", balance > desiredAmount)
-    return BigNumber.from(balance).gte(BigNumber.from(desiredAmount));
+    return BigNumber.from(balance? balance : 0).gte(BigNumber.from(desiredAmount? desiredAmount: 0));
 }
